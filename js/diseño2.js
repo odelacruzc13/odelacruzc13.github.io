31b7bd984f72e35733bb1db9c4dbbbe7927b18b4
oscarelias_93 <oscar_worker@hotmail.com>
2017-06-05 13:17:43 -0500

    v100

--- js/diseño2.js
+++ js/diseño2.js
@@ -1,15 +1,11 @@
-function changeImage() {
-	var pokeballImg = document.getElementById("icon01");
-	pokeballImg.src="img/pika.gif";
+
+function() {
+	window.onload = function() {
+		function changeImage() {
+			var pokeballImg = document.getElementById("icon01");
+			pokeballImg.src="img/pika.gif"
+				}
+}
 }
 
-function pageLoad() {
-	var  ok = document.getElementById("icon01");
-	ok.onclick=okayClick;
-}
 
-function okayClick() {
-	alert("booya");
-}
-
-window.onLoad = pageLoad;
