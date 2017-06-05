5ce61e6deae9bb18c1f17d837e7ae0873043000a
oscarelias_93 <oscar_worker@hotmail.com>
2017-06-05 13:03:15 -0500

    v100

--- js/diseño2.js
+++ js/diseño2.js
@@ -2,3 +2,14 @@ function changeImage() {
 	var pokeballImg = document.getElementById("icon01");
 	pokeballImg.src="img/pika.gif";
 }
+
+function pageLoad() {
+	var  ok = document.getElementById("icon01");
+	ok.onclick=okayClick;
+}
+
+function okayClick() {
+	alert("booya");
+}
+
+window.onLoad = pageLoad;
